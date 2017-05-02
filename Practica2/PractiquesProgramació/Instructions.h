2ecd47595df5a8d00a48c2ea077564eb13ad0b61
Bernat <bernatriupuyal@enti.cat>
2017-05-02 18:30:27 +0200

    you can call me cazy

---
+++ Practica2/PractiquesProgramació/Instructions.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Instructions.h"
+#include <fstream>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <algorithm>
+#include <cstdlib>
+
+void add(std::vector<std::string> &llista, int nombre);
+void addBasics(std::vector<std::string> &llista);
+void deleteFunct(std::vector<std::string> &llista, int nombre);
+void info(std::vector<std::string> llista, int nombre);
+void sort(std::vector<std::string> &llista);
+void clean(std::vector<std::string> &llista);
+void help();
